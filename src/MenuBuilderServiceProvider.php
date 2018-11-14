0840792686a26758f21750d32410fa3c5bb12ca4
Joel Haker <joel.haker@greenbarlabs.com>
2018-11-14 11:19:12 -0500

    WIP stubs out blade directive to build the menu

--- src/MenuBuilderServiceProvider.php
+++ src/MenuBuilderServiceProvider.php
@@ -4,6 +4,7 @@ namespace GreenBar\MenuBuilder;
 
 use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Compilers\BladeCompiler;
 
 class MenuBuilderServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,11 @@ class MenuBuilderServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // $this->loadViewsFrom(__DIR__.'/resources/views/partials', 'menu_builder');
+        // $this->publishes([
+        //     __DIR__.'/resources/views/partials' => resource_path('views/vendor/meubuilder'),
+        // ]);
+
         $this->publishes([
             __DIR__.'/../config/menu_builder.php' => config_path('menu_builder.php'),
         ], 'config');
@@ -52,5 +58,27 @@ class MenuBuilderServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__.'/../config/menu_builder.php', 'menu_builder'
         );
+
+        // $this->registerBladeExtensions();
+    }
+
+    /**
+     * The package's blade extensions
+     */
+    protected function registerBladeExtensions()
+    {
+        // $this->app->afterResolving('blade.compiler', function (BladeCompiler $bladeCompiler) {
+        //     $bladeCompiler->directive('menu_bulder', function ($arguments) {
+        //         $menu_class = config('menu_builder.models.menu');
+        //         $menu_item_class = config('menu_builder.models.menu_item');
+
+        //         list($menu_position, $classes) = explode(',', $arguments.',');
+                
+        //         $menu = $menu_class::where()
+
+        //         $partial_template = view('menu_builder::main_navigation', $menu_position);
+        //         return $partial_template->render();
+        //     });
+        // });
     }
 }
